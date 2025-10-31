15cc97434ecff60ef4c159cedb43addbe5978dd8
BernardoSoares303 <bbnado34@gmail.com>
2025-10-31 20:51:33 -0300

    Nome da Pasta Alterado

--- Trabalho_MS/Properties/Resources.Designer.cs
+++ Trabalho_MS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Trabalho_MS.Properties {
+namespace LabuStore.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Trabalho_MS.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Trabalho_MS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LabuStore.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
